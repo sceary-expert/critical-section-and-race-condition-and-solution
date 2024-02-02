16b3414e654f98401eb4c3a0704a7dac6b07fd99
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2024-02-02 11:16:41 +0530

    Update atomic-operations.cpp

--- atomic-operations.cpp
+++ atomic-operations.cpp
@@ -7,17 +7,17 @@ std::atomic<int> sharedResource(0);
 void criticalSection(int threadId) {
     for (int i = 0; i < 5; ++i) {
         // Entry section - value before critical section
-        int beforeValue = sharedResource.load();
+        // int beforeValue = sharedResource.load();
         // std::cout << "Thread " << threadId << " before critical section. Shared resource value: " << beforeValue << std::endl;
-        cout<<beforeValue<<endl;
+        // cout<<beforeValue<<endl;
 
         // Critical section - atomic operation to update shared resource
         sharedResource.fetch_add(1);
 
         // Exit section - value after critical section
-        int afterValue = sharedResource.load();
+        // int afterValue = sharedResource.load();
         // std::cout << "Thread " << threadId << " after critical section. Shared resource value: " << afterValue << std::endl;
-        cout<<afterValue<<endl;
+        // cout<<afterValue<<endl;
 
         // Simulate some work in the critical section
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -32,29 +32,12 @@ int main() {
     // Wait for threads to finish
     thread1.join();
     thread2.join();
+    int changedValue = sharedResource.load();
+    cout<<changedValue<<endl;
 
     return 0;
 }
 
 /*
-0
-1
-0
-2
-2
-23
-
-4
-44
-
-5
-6
-66
-
-7
-8
-8
-89
-
 10
 */
